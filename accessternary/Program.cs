638a1b37d99d51834d5979f9ca28135152fd7476
Robert Farfsing <robertfarfsing@gmail.com>
2020-02-10 16:29:09 -0500

    public and private library and project

---
+++ accessternary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AccessTernary {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine($"The Cube of 17 is {Program.cubed(17)}");
+            Console.WriteLine(Customer.PrintNextId());
+            var cust1 = new Customer("Max Technical Training");
+            var cust2 = new Customer("Amazon");
+            var cust3 = new Customer("Target");
+            cust1.NationalAccount("yes");
+            var order1 = new Order { Id = 1, Amount = 1000, Customer = cust1 };
+
+        }
+        static int cubed(int nbr) {
+            return nbr * nbr * nbr;
+        }
+    }
+}
---
+++ saleslibrary/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccessTernary {
+    public class Customer {
+        private static int NextId = 1;
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        private bool IsNationalAccount { get; set; }
+        public void NationalAccount(string YesOrNo) {
+            this.IsNationalAccount = YesOrNo.Equals("yes") ? true : false;
+        }
+        public static string PrintNextId() {
+            return ($"Next id is {NextId}");
+        }
+        public Customer(string Name) {
+            this.Name = Name;
+            this.Id = NextId;
+            NextId++;
+        }
+    }
+}
